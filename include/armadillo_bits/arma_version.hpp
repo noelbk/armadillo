8f211408e3bcd4c03132648adfff4d939a2ba0bb
conrad <conrad@localhost.localdomain>
2019-06-27 19:12:08 +1000

    mark as unstable

--- include/armadillo_bits/arma_version.hpp
+++ include/armadillo_bits/arma_version.hpp
@@ -20,9 +20,9 @@
 
 
 #define ARMA_VERSION_MAJOR 9
-#define ARMA_VERSION_MINOR 500
-#define ARMA_VERSION_PATCH 3
-#define ARMA_VERSION_NAME  "Riot Compact"
+#define ARMA_VERSION_MINOR 599
+#define ARMA_VERSION_PATCH 0
+#define ARMA_VERSION_NAME  "unstable"
 
 
 
