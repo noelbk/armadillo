c9a95bf33e4f12e9cc9c00b8844009ad5bcbf378
conrad <conrad@localhost.localdomain>
2019-10-23 12:15:08 +1000

    allow ARMA_USE_EXTERN_CXX11_RNG on macOS

--- include/armadillo_bits/compiler_setup.hpp
+++ include/armadillo_bits/compiler_setup.hpp
@@ -142,8 +142,8 @@
   // #undef  ARMA_HAVE_POSIX_MEMALIGN
   // NOTE: posix_memalign() is available since macOS 10.6 (late 2009 onwards)
   
-  #undef  ARMA_USE_EXTERN_CXX11_RNG
-  // TODO: thread_local seems to work in Apple clang since Xcode 8 (mid 2016 onwards)
+  // #undef  ARMA_USE_EXTERN_CXX11_RNG
+  // NOTE: thread_local seems to work in Apple clang since Xcode 8 (mid 2016 onwards)
   // NOTE: https://stackoverflow.com/questions/28094794/why-does-apple-clang-disallow-c11-thread-local-when-official-clang-supports
 #endif
 
